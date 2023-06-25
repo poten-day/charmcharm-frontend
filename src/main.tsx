171c78dd792f82e10ace7b363515da796017934e
도비 <jm.dev.study@gmail.com>
2023-06-25 18:09:24 +0900

    Chore: msw 설치 및 환경 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = await import('./mocks/browser.ts');
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
