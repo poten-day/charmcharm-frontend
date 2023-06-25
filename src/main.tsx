d5c4d1359274b5ff3973ac6df52d87b49440f303
도비 <jm.dev.study@gmail.com>
2023-06-26 00:49:04 +0900

    Fix: worker.start의 onUnhandledRequest 옵션 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 if (process.env.NODE_ENV === 'development') {
   const { worker } = await import('./mocks/browser.ts');
-  worker.start();
+  worker.start({ onUnhandledRequest: 'bypass' });
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
