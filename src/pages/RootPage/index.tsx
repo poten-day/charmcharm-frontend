9305712c412d802912617ad245a476ff92a4de63
도비 <jm.dev.study@gmail.com>
2023-06-26 13:55:32 +0900

    Feat: root 페이지의 대략적인 ui 배치 추가

--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -1,5 +1,17 @@
 const RootPage = () => {
-  return <div>Root</div>;
+  return (
+    <div>
+      <div>로고</div>
+      <div>이미지</div>
+      <form className="flex flex-col">
+        <label htmlFor="typingName">
+          <p>내 친구들은 나를 어떻게 생각할까?</p>
+        </label>
+        <input id="typingName" type="text" placeholder="이름을 입력해주세요" />
+        <button type="button">시작하기</button>
+      </form>
+    </div>
+  );
 };
 
 export default RootPage;
