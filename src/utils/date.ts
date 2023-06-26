7319597a71af82cfe8f1c74e7cdd71fb27f21e63
도비 <jm.dev.study@gmail.com>
2023-06-27 01:05:51 +0900

    Fix: updateTitleByTime 함수의 내부 변수명 변경

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -14,15 +14,15 @@ export const millisecondsToTime = (milliseconds: number) => {
 };
 
 export const updateTitleByTime = (targetTime: string) => {
-  const createTime = dayjs(targetTime).subtract(4, 'hour').add(5, 'minute').format();
-  const last5Minute = dayjs(targetTime).subtract(5, 'minute');
+  const createTimeAfter5Minute = dayjs(targetTime).subtract(4, 'hour').add(5, 'minute').format();
+  const targetTimeLast5Minute = dayjs(targetTime).subtract(5, 'minute');
   const currentTime = dayjs(new Date());
 
-  if (diffTime(createTime) > 0) {
+  if (diffTime(createTimeAfter5Minute) > 0) {
     return '매력찾기 설문조사 시작-!';
   }
 
-  if (last5Minute.diff(currentTime) < 0) {
+  if (targetTimeLast5Minute.diff(currentTime) < 0) {
     return '시간이 얼마 남지 않았어요';
   }
 
