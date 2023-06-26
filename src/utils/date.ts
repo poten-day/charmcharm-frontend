b6e4d46e32627d6519ac3176c8a5c1ee2a9a0127
도비 <jm.dev.study@gmail.com>
2023-06-27 00:52:57 +0900

    Feat: 시간에 따라 타이틀의 텍스트를 바꾸는 함수 구현

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -13,4 +13,18 @@ export const millisecondsToTime = (milliseconds: number) => {
   return dayjs.duration(milliseconds);
 };
 
+export const updateTitleByTime = (targetTime: string) => {
+  const createTime = dayjs(targetTime).subtract(4, 'hour').add(5, 'minute').format();
+  const last5Minute = dayjs(targetTime).subtract(5, 'minute');
+  const currentTime = dayjs(new Date());
+
+  if (diffTime(createTime) > 0) {
+    return '매력찾기 설문조사 시작-!';
+  }
+
+  if (last5Minute.diff(currentTime) < 0) {
+    return '시간이 얼마 남지 않았어요';
+  }
+
+  return '아직 설문조사 진행중-!';
 };
