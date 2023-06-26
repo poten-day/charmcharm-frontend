a6f1454bea187b568c1960ac5777138c47953c0f
도비 <jm.dev.study@gmail.com>
2023-06-27 01:19:48 +0900

    Refactor: diffTime 함수를 재사용성 있게 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,10 +3,10 @@ import duration from 'dayjs/plugin/duration';
 
 dayjs.extend(duration);
 
-export const diffTime = (targetTime: string) => {
-  const openTime = dayjs(targetTime);
-  const currentTime = dayjs(new Date());
-  return openTime.diff(currentTime);
+export const diffTime = (target1: string | Date, target2: string | Date) => {
+  const target1Time = dayjs(target1);
+  const target2Time = dayjs(target2);
+  return target1Time.diff(target2Time);
 };
 
 export const millisecondsToTime = (milliseconds: number) => {
@@ -15,14 +15,15 @@ export const millisecondsToTime = (milliseconds: number) => {
 
 export const updateTitleByTime = (targetTime: string) => {
   const createTimeAfter5Minute = dayjs(targetTime).subtract(4, 'hour').add(5, 'minute').format();
-  const targetTimeLast5Minute = dayjs(targetTime).subtract(5, 'minute');
-  const currentTime = dayjs(new Date());
+  const targetTimeLast5Minute = dayjs(targetTime).subtract(5, 'minute').format();
+  const currentTime = new Date();
+  const diffEndTime = dayjs(targetTime).diff(currentTime);
 
-  if (diffTime(createTimeAfter5Minute) > 0) {
+  if (diffTime(createTimeAfter5Minute, new Date()) > 0) {
     return '매력찾기 설문조사 시작-!';
   }
 
-  if (targetTimeLast5Minute.diff(currentTime) < 0) {
+  if (diffTime(targetTimeLast5Minute, currentTime) < 0 && diffEndTime > 0) {
     return '시간이 얼마 남지 않았어요';
   }
 
