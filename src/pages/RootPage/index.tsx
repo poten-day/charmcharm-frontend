43eba2aebe2f390d65b7377e54519f1640358706
도비 <jm.dev.study@gmail.com>
2023-06-26 14:19:38 +0900

    Feat: 이름 입력 input의 글자수 제한 설정 - 한글 입력에 대한 입력 오류 방지

--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+
+const MAX_LENGTH = 5;
+
 const RootPage = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const inputTyping = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > MAX_LENGTH) {
+      e.target.value = e.target.value.slice(0, MAX_LENGTH);
+    }
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
       <div>로고</div>
@@ -7,7 +20,12 @@ const RootPage = () => {
         <label htmlFor="typingName">
           <p>내 친구들은 나를 어떻게 생각할까?</p>
         </label>
-        <input id="typingName" type="text" placeholder="이름을 입력해주세요" />
+        <input
+          id="typingName"
+          type="text"
+          placeholder="이름을 입력해주세요"
+          onChange={inputTyping}
+        />
         <button type="button">시작하기</button>
       </form>
     </div>
