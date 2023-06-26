750a7670d36c8c58381bdd3e8d2ba4aca27b3036
도비 <jm.dev.study@gmail.com>
2023-06-26 15:36:53 +0900

    Refactor: 카운트다운과 관련된 영역을 CountdownArea 컴포넌트로 분리

---
+++ src/components/CountdownArea/index.tsx
@@ -0,0 +1,27 @@
+import { useLocation } from 'react-router-dom';
+
+const CountdownArea = () => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <h1>상황에 따라 바뀌는 메인 텍스트</h1>
+      <p>
+        설문조사는 <strong>단 4시간 동안</strong> 진행됩니다.
+        <br /> 제한시간 내 친구들에게 링크를 공유해주세요!
+      </p>
+      <div className="flex items-center gap-2">
+        <span>아이콘</span>
+        <p>남은 시간</p>
+      </div>
+      <div>
+        <input type="text" defaultValue={`https://charmcharm.me${location.pathname}`} readOnly />
+        <button type="button">공유</button>
+      </div>
+      <button type="button">설문 시작하기</button>
+      <p>00:00에 결과가 오픈됩니다!</p>
+    </div>
+  );
+};
+
+export default CountdownArea;
--- src/pages/IdPage/index.tsx
+++ src/pages/IdPage/index.tsx
@@ -1,8 +1,6 @@
-import { useLocation } from 'react-router-dom';
+import CountdownArea from '@/components/CountdownArea';
 
 const IdPage = () => {
-  const location = useLocation();
-
   return (
     <div className="text-center">
       <header>나의 매력을 알려줘</header>
@@ -13,23 +11,7 @@ const IdPage = () => {
           메모장에 꼭 저장해두세요!
         </p>
       </div>
-      <div>
-        <h1>상황에 따라 바뀌는 메인 텍스트</h1>
-        <p>
-          설문조사는 <strong>단 4시간 동안</strong> 진행됩니다.
-          <br /> 제한시간 내 친구들에게 링크를 공유해주세요!
-        </p>
-        <div className="flex items-center gap-2">
-          <span>아이콘</span>
-          <p>남은 시간</p>
-        </div>
-        <div>
-          <input type="text" defaultValue={`https://charmcharm.me${location.pathname}`} readOnly />
-          <button type="button">공유</button>
-        </div>
-        <button type="button">설문 시작하기</button>
-        <p>00:00에 결과가 오픈됩니다!</p>
-      </div>
+      <CountdownArea />
     </div>
   );
 };
