d017fac5d200b193cd5417374ca371d9db8d4ab7
도비 <jm.dev.study@gmail.com>
2023-06-27 00:18:21 +0900

    Feat: millisecond를 day.js 객체로 만드는 함수 구현

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,16 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
 
 export const diffTime = (targetTime: string) => {
   const openTime = dayjs(targetTime);
   const currentTime = dayjs(new Date());
   return openTime.diff(currentTime);
 };
+
+export const millisecondsToTime = (milliseconds: number) => {
+  return dayjs.duration(milliseconds);
+};
+
 };
