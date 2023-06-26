0459340f8fcf4559582986ac2fe92617409c400c
도비 <jm.dev.study@gmail.com>
2023-06-27 01:53:06 +0900

    Feat: /api/charmers 요청에 대한 mock 함수 생성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,29 @@
 import { rest } from 'msw';
+import dayjs from 'dayjs';
+
+const MOCK_USER_DATA = [
+  {
+    name: 'dobby',
+    openTime: '2023-06-27T01:47:00.000000',
+    shareLink: 'https://charmcharm.me/test12345',
+  },
+  {
+    name: '김도비',
+    openTime: '2023-06-27T02:04:11.379398',
+    shareLink: 'https://charmcharm.me/testuser13123123442',
+  },
+];
 
 export const handlers = [
-  rest.get('/test', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ message: 'test success' }));
+  rest.post('/api/charmers', async (req, res, ctx) => {
+    const id = req.id;
+    const name = await req.text();
+    const responseData = {
+      name,
+      openTime: dayjs().add(4, 'hour').format(),
+      shareLink: `https://charmcharm.me/${id}`,
+    };
+    MOCK_USER_DATA.push(responseData);
+    return res(ctx.status(200), ctx.json(responseData));
   }),
 ];
