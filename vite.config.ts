d5e5daa1465dc324608635eed88ef285301dcb86
도비 <jm.dev.study@gmail.com>
2023-06-27 11:51:58 +0900

    Chore: vite-plugin-mkcert 설치 및 환경 설정 - https 환경으로 실행 가능

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
@@ -17,5 +18,8 @@ export default defineConfig({
       { find: '@globalTypes', replacement: path.resolve(__dirname, 'src/types') },
       { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
     ],
+  },
+  server: {
+    https: true,
   },
 });
