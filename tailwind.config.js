96c8aff2e1a1fbfc1d291d993dcf35d2cc4cc1a2
도비 <jm.dev.study@gmail.com>
2023-06-28 23:46:43 +0900

    Chore: tailwind colors, fontSize 옵션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary500: '#5E31FE',
+      primary100: '#EFEBFF',
+      gray900: '#1B1B1B',
+      gray500: '#AAA',
+      gray200: '#F1F1F1',
+      black: '#000000',
+      white: '#FFFFFF',
+      system: '#CD3440',
+    },
+    fontSize: {
+      baseReguler: ['14px', { lineHeight: '140%' }],
+      baseMedium: ['14px', { lineHeight: '140%', fontWeight: 500 }],
+      baseSemibold: ['14px', { lineHeight: '130%', fontWeight: 600 }],
+      lgReguler: ['18px', { lineHeight: '140%' }],
+      lgMedium: ['18px', { lineHeight: '140%', fontWeight: 600 }],
+      lgSemibold: ['18px', { lineHeight: '130%', fontWeight: 500 }],
+      xlReguler: ['20px', { lineHeight: '140%' }],
+      xlMedium: ['20px', { lineHeight: '140%', fontWeight: 500 }],
+      xlSemibold: ['20px', { lineHeight: '130%', fontWeight: 600 }],
+      h1: ['48px', { lineHeight: '110%' }],
+      h2: ['32px', { lineHeight: '110%' }],
+      h3: ['20px', { lineHeight: '130%' }],
+      smTimer: ['16px', { lineHeight: '16px' }],
+      lgTimer: ['40px', { lineHeight: '70px' }],
+    },
     extend: {
       fontFamily: {
         pretendard: ['pretendard'],
