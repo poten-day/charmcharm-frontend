62639babdd62a3139b36eec79e014adcc9bc5ddf
도비 <jm.dev.study@gmail.com>
2023-06-28 17:21:34 +0900

    Feat: 결과 요청 쿼리 작성

--- src/components/Result/ResultContainer/index.tsx
+++ src/components/Result/ResultContainer/index.tsx
@@ -1,7 +1,16 @@
+import { getCharmersResult } from '@/api';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 import MainResult from '../MainResult';
 import SubResult from '../SubResult';
 
 const ResultContainer = () => {
+  const params = useParams();
+  const { data } = useQuery({
+    queryKey: ['user-result', params.id],
+    queryFn: () => getCharmersResult(params.id),
+  });
+
   return (
     <div>
       <MainResult />
