ed77b7927403e126aaa8d4d370ccf7c8af48cfbb
도비 <jm.dev.study@gmail.com>
2023-06-28 16:14:51 +0900

    Refactor: 결과에 대한 영역을 MainResult와 SubResult로 분리

---
+++ src/components/Result/MainResult/index.tsx
@@ -0,0 +1,27 @@
+const MainResult = () => {
+  return (
+    <div>
+      <p>
+        친구들이 말하는 <br />
+        name의 매력은?
+      </p>
+      <div>
+        <p>메인 컨셉</p>
+      </div>
+      <p>서브컨셉1, 서브컨셉2</p>
+      <p>이런 너를 생각하면 oo색이 떠올라!</p>
+      <ul>
+        <li>
+          <p>친구들이 말하는 너는</p>
+          <p>ㅇㅇ,ㅇㅇ,ㅇㅇ</p>
+        </li>
+        <li>
+          <p>친구들에게 너는</p>
+          <p>같이 ㅇㅇ하고 싶은 사람</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MainResult;
---
+++ src/components/Result/SubResult/index.tsx
@@ -0,0 +1,14 @@
+const SubResult = () => {
+  return (
+    <div>
+      <p>친구들의 답변 전체</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi ducimus, quo quis labore
+        sapiente minima officiis molestiae veritatis provident placeat optio pariatur harum
+        recusandae culpa odio, minus voluptates dicta quos.
+      </p>
+    </div>
+  );
+};
+
+export default SubResult;
