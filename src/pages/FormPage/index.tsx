85ac5a95d0112c88f440de86e6015c93557755f4
도비 <jm.dev.study@gmail.com>
2023-06-28 12:51:40 +0900

    Feat: useUserStore에 정보가 존재하지 않으면 /:id 페이지로 이동

--- src/pages/FormPage/index.tsx
+++ src/pages/FormPage/index.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useUserStore } from '@/store/useUserStore';
 import QuestionsContainer from '@components/Questions/QuestionsContainer';
 
 const FormPage = () => {
+  const navigate = useNavigate();
+  const param = useParams();
+  const { user } = useUserStore();
+
+  useEffect(() => {
+    if (user.name === '') {
+      navigate(`/${param.id}`);
+    }
+  }, []);
+
   return <QuestionsContainer />;
 };
 
