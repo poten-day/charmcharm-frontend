bb37c6009c2ccb64b38a02b461a5a644f561fafe
도비 <jm.dev.study@gmail.com>
2023-06-28 23:45:02 +0900

    Chore: font assets 추가 및 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ['pretendard'],
+        BRBA_B: ['BRBA_B'],
+        NeoDunggeunmoPro: ['NeoDunggeunmoPro'],
+      },
+    },
   },
   plugins: [],
 };
