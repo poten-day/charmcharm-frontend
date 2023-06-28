b1eb9214b5fc6a60411f9aa802381b7a74c4b7e6
도비 <jm.dev.study@gmail.com>
2023-06-28 16:09:40 +0900

    Refactor: ResultPage 내부 태그를 ResultContainer로 분리

---
+++ src/components/Result/ResultContainer/index.tsx
@@ -0,0 +1,42 @@
+const ResultContainer = () => {
+  return (
+    <div>
+      <header>헤더</header>
+      <div>
+        <p>
+          친구들이 말하는 <br />
+          name의 매력은?
+        </p>
+        <div>
+          <p>메인 컨셉</p>
+        </div>
+        <p>서브컨셉1, 서브컨셉2</p>
+        <p>이런 너를 생각하면 oo색이 떠올라!</p>
+        <ul>
+          <li>
+            <p>친구들이 말하는 너는</p>
+            <p>ㅇㅇ,ㅇㅇ,ㅇㅇ</p>
+          </li>
+          <li>
+            <p>친구들에게 너는</p>
+            <p>같이 ㅇㅇ하고 싶은 사람</p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>친구들의 답변 전체</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi ducimus, quo quis labore
+          sapiente minima officiis molestiae veritatis provident placeat optio pariatur harum
+          recusandae culpa odio, minus voluptates dicta quos.
+        </p>
+      </div>
+      <div>
+        <button>저장하기</button>
+        <button>공유하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default ResultContainer;
