9312ee4133f1b30d519d0d8ad297ad13246d7cc6
도비 <jm.dev.study@gmail.com>
2023-06-29 03:19:28 +0900

    Chore: storybook 설치 및 관련 애드온 세팅, 환경 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,19 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': 'error',
-  },
+    'prettier/prettier': 'error'
+  }
 };
