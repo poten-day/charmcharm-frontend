e25787435cae368baa5df20d16e1dae02ecc32fc
도비 <jm.dev.study@gmail.com>
2023-06-28 10:40:14 +0900

    Feat: 답변 제출에 대한 api 요청 함수 생성

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { CharmersDefaultType, CharmersExtendsType, CharmersQuestionType } from './types';
+import { AnswersFormType } from '@/store/useUserStore';
 
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_BASE_URL}`,
@@ -19,6 +20,17 @@ export const getCharmers = async (id: string): Promise<CharmersExtendsType> => {
 
 export const getQuestionForm = async (id: string | undefined): Promise<CharmersQuestionType> => {
   const { data } = await instance.get(`/api/questions/${id}`);
+  return data;
+};
+
+export const postAnswersForm = async ({
+  id,
+  answers,
+}: {
+  id: string | undefined;
+  answers: AnswersFormType[];
+}) => {
+  const { data } = await instance.post(`/api/answers/${id}`, answers);
   return data;
 };
 
