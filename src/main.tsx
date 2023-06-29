a6853399ef30faae176a711da592fa6937a6da76
도비 <jm.dev.study@gmail.com>
2023-06-29 21:22:58 +0900

    Fix: msw 설정 주석 처리

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = await import('./mocks/browser.ts');
-  worker.start({ onUnhandledRequest: 'bypass' });
-}
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = await import('./mocks/browser.ts');
+//   worker.start({ onUnhandledRequest: 'bypass' });
+// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
