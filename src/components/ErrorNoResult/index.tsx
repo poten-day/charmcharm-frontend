368328ec5862d4763566ad575680ae6ff18ce7ae
도비 <jm.dev.study@gmail.com>
2023-06-29 20:01:38 +0900

    Design: ErrorNoResult 컴포넌트 스타일 적용 및 공용 컴포넌트 추가

--- src/components/ErrorNoResult/index.tsx
+++ src/components/ErrorNoResult/index.tsx
@@ -1,23 +1,26 @@
-import Timer from '@/components/Common/Timer';
-import useSetRouter from '@/hooks/useSetRouter';
+import useSetRouter from '@hooks/useSetRouter';
+import Header from '@components/Common/Header';
+import Timer from '@components/Common/Timer';
+import Button from '@components/Common/Button';
 
 const ErrorNoResult = () => {
   const { routerHelper } = useSetRouter();
   const onClickNavigateMain = () => routerHelper.main();
 
   return (
-    <div>
-      <header>헤더</header>
-      <p>ㅠ.ㅠ</p>
-      <p>
-        제한 시간이 <br /> 종료되었어요.
-      </p>
-      <p>아직 답변이 기록되지 않았어요.</p>
-      <Timer time={0} />
-      <button type="button" onClick={onClickNavigateMain}>
-        새로운 시작하기
-      </button>
-    </div>
+    <>
+      <Header />
+      <div className="text-center flex flex-col gap-[40px] py-[40px] px-[20px]">
+        <div>
+          <p className="text-h1 font-BRBA_B text-gray900 mb-[20px]">
+            <b className="text-primary500">ㅠ.ㅠ</b> <br /> 제한 시간이 <br /> 종료되었어요.
+          </p>
+          <p className="text-baseReguler text-gray900">아직 답변이 기록되지 않았어요.</p>
+        </div>
+        <Timer time={0} />
+        <Button label="새로운 시작하기" onClick={onClickNavigateMain} />
+      </div>
+    </>
   );
 };
 
