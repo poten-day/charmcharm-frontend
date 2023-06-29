44e2b1edca18580f03ae3ad44138717f4ee42c67
도비 <jm.dev.study@gmail.com>
2023-06-29 19:01:20 +0900

    Design: 답변 성공 페이지 스타일 적용

--- src/pages/FormPage/SuccessPage/index.tsx
+++ src/pages/FormPage/SuccessPage/index.tsx
@@ -2,7 +2,11 @@ import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import useSetTimes from '@/hooks/useSetTimes';
 import { useUserStore } from '@/store/useUserStore';
-import ShareURL from '@/components/Common/ShareURL';
+
+import ShareURL from '@components/Common/ShareURL';
+import Header from '@components/Common/Header';
+import Guideline from '@components/Common/Guideline';
+import Button from '@components/Common/Button';
 
 const FormSuccessPage = () => {
   const navigate = useNavigate();
@@ -41,16 +45,30 @@ const FormSuccessPage = () => {
   }, []);
 
   return (
-    <div>
-      <header>나의 매력을 알려줘</header>
-      <p>링크저장하세요</p>
-      <p>{user.name}에게 소중한 답변이 전달되었어요.</p>
-      <p>{finishedTime}에 결과가 오픈됩니다!</p>
-      <ShareURL url={user.shareLink} />
-      <button type="button" onClick={onClickNavigateMain}>
-        내 매력도 알아보기
-      </button>
-    </div>
+    <>
+      <Header />
+      <Guideline />
+      <div className="text-center px-[20px]">
+        <div className="my-[40px]">
+          <p className="text-[40px]">💌</p>
+          <p className="text-h1 font-BRBA_B my-[20px]">
+            {user.name}에게 <br />
+            소중한 답변이 <br />
+            전달되었어요.
+          </p>
+          <p className="text-baseReguler text-gray500">{finishedTime}에 결과가 오픈됩니다!</p>
+        </div>
+        <div className="flex flex-col gap-[20px]">
+          <ShareURL url={user.shareLink} />
+          <Button
+            color="black"
+            label="내 매력도 알아보기"
+            onClick={onClickNavigateMain}
+            size="large"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
