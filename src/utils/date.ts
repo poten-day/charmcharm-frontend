11c4f4a25c464f43e51e1e9b0e932f4bbf2bbe1a
도비 <jm.dev.study@gmail.com>
2023-06-29 19:49:04 +0900

    Fix: updateTitleByTime 리턴값에 띄어쓰기 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -20,16 +20,16 @@ export const updateTitleByTime = (targetTime: string) => {
   const diffEndTime = dayjs(targetTime).diff(currentTime);
 
   if (diffTime(createTimeAfter5Minute, new Date()) > 0) {
-    return '매력찾기 설문조사 시작-!';
+    return '매력찾기\r\n설문조사 시작-!';
   }
 
   if (diffTime(targetTimeLast5Minute, currentTime) < 0 && diffEndTime > 0) {
-    return '시간이 얼마 남지 않았어요';
+    return '시간이 얼마\r\n남지 않았어요';
   }
 
   if (diffTime(targetTime, currentTime) <= 0) {
-    return '제한시간이 종료되었어요';
+    return '제한시간이\r\n종료되었어요';
   }
 
-  return '아직 설문조사 진행중-!';
+  return '아직 설문조사\r\n진행중-!';
 };
