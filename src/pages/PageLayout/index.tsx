477e9641876e06208ce55e03ce966eef669b0f43
도비 <jm.dev.study@gmail.com>
2023-06-29 21:20:43 +0900

    Design: 백그라운드 색상 변경

--- src/pages/PageLayout/index.tsx
+++ src/pages/PageLayout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const PageLayout = () => {
   return (
-    <div className="h-screen bg-primary100">
+    <div className="h-screen">
       <div className="max-w-[480px] bg-white h-screen m-auto">
         <Outlet />
       </div>
