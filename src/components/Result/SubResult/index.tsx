bf7b9eb5f657a6dfa1edd2e6b668a9709f954c2c
도비 <jm.dev.study@gmail.com>
2023-06-29 19:19:36 +0900

    Design: SubResult 컴포넌트 스타일 적용

--- src/components/Result/SubResult/index.tsx
+++ src/components/Result/SubResult/index.tsx
@@ -1,17 +1,25 @@
-import { AnswersSubQuestionType } from '@/api/types';
+import { AnswersResultCountType, AnswersSubQuestionType } from '@/api/types';
 
 const SubResult = ({ data }: { data: AnswersSubQuestionType[] }) => {
+  const helper = (answers: AnswersResultCountType[]) => {
+    return answers
+      .map((answer) => {
+        return `${answer.answerName}(${answer.answerCount}명)`;
+      })
+      .join(', ');
+  };
+
   return (
-    <div>
+    <div className="px-[20px] py-[32px] bg-[#F8F8F8] text-gray500 flex flex-col gap-[20px]">
       <p>친구들의 답변 전체</p>
-      {data.map((data) => (
+      {data.map((data, index) => (
         <div key={`answers-${data.questionId}`}>
-          <p>{data.questionTitle}</p>
-          {data.answerResults.map((result) => (
-            <span key={`${result.answerId}-${result.answerName}`}>
-              {result.answerName}({result.answerCount}명)
-            </span>
-          ))}
+          <p>
+            {index + 1}. {data.questionTitle}
+          </p>
+          <div>
+            <span>: {helper(data.answerResults)}</span>
+          </div>
         </div>
       ))}
     </div>
