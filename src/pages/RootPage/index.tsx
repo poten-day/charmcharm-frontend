b25ab102667784d8afc03c8c98e8ddc8c31a0259
도비 <jm.dev.study@gmail.com>
2023-06-29 16:49:11 +0900

    Feat: root 페이지 이미지 추가

--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -2,11 +2,17 @@ import StartForm from '@/components/StartForm';
 
 const RootPage = () => {
   return (
-    <div>
-      <div>로고</div>
-      <div>이미지</div>
+    <>
+      <img
+        className="absolute"
+        src="/assets/images/logo/smlogo-white.svg"
+        alt="참참 로고"
+        width={480}
+        height={72}
+      />
+      <img src="/assets/images/main.png" alt="나의 매력을 알려줘" width={480} height={500} />
       <StartForm />
-    </div>
+    </>
   );
 };
 
