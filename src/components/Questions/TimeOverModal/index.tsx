1e024dcbe1b03982fe37a563d3a6e6fd97250114
도비 <jm.dev.study@gmail.com>
2023-06-29 20:14:32 +0900

    Design: TimeOverModal 스타일 적용 및 공용 컴포넌트 추가

--- src/components/Questions/TimeOverModal/index.tsx
+++ src/components/Questions/TimeOverModal/index.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Common/Button';
 import useSetRouter from '@/hooks/useSetRouter';
 import { useUserStore } from '@/store/useUserStore';
 
@@ -6,15 +7,14 @@ const TimeOverModal = () => {
   const { routerHelper } = useSetRouter();
 
   return (
-    <div className="absolute bg-slate-300">
-      <p>앗 답변 시간이 종료되었어요.</p>
-      <p>
+    <div className="absolute w-[360px] bg-slate-300 mx-[20px] p-[20px] text-center rounded-[8px] border border-gray200">
+      <p className="text-lgSemibold my-[8px]">앗 답변 시간이 종료되었어요.</p>
+      <p className="text-baseReguler text-gray500">
         아쉽게도 완료하지 않은 답변은 <br />
         {user.name}에게 전달되지 않아요
       </p>
-      <button type="button" onClick={() => routerHelper.result()}>
-        결과 확인하기
-      </button>
+      <p className="text-lgSemibold my-[24px] text-[72px]">⏰</p>
+      <Button label="결과 확인하기" onClick={() => routerHelper.result()} />
     </div>
   );
 };
