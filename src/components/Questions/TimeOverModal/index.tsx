dbd03d5fa0077f96db02ca67efb8e2f5874e98be
도비 <jm.dev.study@gmail.com>
2023-06-30 04:38:52 +0900

    Fix: 모달 버튼 외부 영역 클릭시 닫히는 기능 및 일부 수정

--- src/components/Questions/TimeOverModal/index.tsx
+++ src/components/Questions/TimeOverModal/index.tsx
@@ -2,31 +2,31 @@ import Button from '@/components/Common/Button';
 import useSetRouter from '@/hooks/useSetRouter';
 import { useUserStore } from '@/store/useUserStore';
 import { ReactComponent as CloseIcon } from '#/assets/images/icons/mingcute-close-line.svg';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useRef } from 'react';
 
 const TimeOverModal = ({ setTimeOver }: { setTimeOver: Dispatch<SetStateAction<boolean>> }) => {
   const { user } = useUserStore();
   const { routerHelper } = useSetRouter();
 
+  const backgroundRef = useRef<HTMLDivElement>(null);
+
   const onClickOutsideCloseModal = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setTimeOver(false);
+    if (e.target === backgroundRef.current) {
+      setTimeOver(false);
+    }
   };
 
   return (
     <div
       className="absolute w-full h-screen transform -translate-x-1/2 -translate-y-1/2 bg-opacity-25 bg-gray900 z-100 top-1/2 left-1/2"
       onClick={onClickOutsideCloseModal}
+      ref={backgroundRef}
     >
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[320px] bg-slate-300 text-center rounded-[8px] border border-gray200 bg-white z-999 p-[20px]">
-        <button
-          type="button"
-          className="flex justify-end w-full"
-          onClick={() => setTimeOver(false)}
-        >
+        <button type="button" className="absolute right-[20px]" onClick={() => setTimeOver(false)}>
           <CloseIcon className="fill-gray500" />
         </button>
-        <p className="text-lgSemibold my-[8px]">앗, 답변 시간이 종료되었어요.</p>
+        <p className="text-lgSemibold mt-[32px] mb-[8px]">앗, 답변 시간이 종료되었어요.</p>
         <p className="text-baseReguler text-gray500">
           아쉽게도 완료하지 않은 답변은 <br />
           {user.name}에게 전달되지 않아요
