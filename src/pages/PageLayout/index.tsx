ee30818322e18db6e563e7e43dcbfa269efa8879
도비 <jm.dev.study@gmail.com>
2023-06-30 15:20:36 +0900

    Feat: 라우터 이동시 스크롤을 최상단으로 위치하도록 추가

--- src/pages/PageLayout/index.tsx
+++ src/pages/PageLayout/index.tsx
@@ -1,12 +1,16 @@
+import ScrollToTop from '@/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 
 const PageLayout = () => {
   return (
-    <div className="h-screen">
-      <div className="max-w-[480px] bg-white h-screen m-auto">
-        <Outlet />
+    <>
+      <ScrollToTop />
+      <div className="h-screen">
+        <div className="max-w-[480px] bg-white h-screen m-auto">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
