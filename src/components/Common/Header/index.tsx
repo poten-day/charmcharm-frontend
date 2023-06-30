e67452ed36ff4ab232d3ebc65801ba058b4186b7
도비 <jm.dev.study@gmail.com>
2023-06-30 14:15:54 +0900

    Feat: 헤더 클릭시 메인페이지로 이동하는 기능 추가

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,11 +1,14 @@
 import { ReactComponent as Vector } from '#/assets/images/vactor/header-vector.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="flex items-baseline gap-[10px] max-w-[480px] justify-center bg-primary500 p-[17px] text-white text-h3 font-BRBA_B">
-      <Vector />
-      나의 매력을 알려줘!
-      <Vector />
+    <header className="max-w-[480px] bg-primary500 p-[17px] text-white text-h3 font-BRBA_B">
+      <Link to={'/'} className="flex items-baseline justify-center gap-[10px]">
+        <Vector />
+        나의 매력을 알려줘!
+        <Vector />
+      </Link>
     </header>
   );
 };
