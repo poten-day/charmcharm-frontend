d46c2c6f3e7d0161af79f063ccf3200c235d465a
도비 <jm.dev.study@gmail.com>
2023-06-30 21:47:08 +0900

    Fix: 카카오톡 인앱 브라우저 조건 변경

--- src/hooks/useShareURL.tsx
+++ src/hooks/useShareURL.tsx
@@ -9,6 +9,13 @@ const useShareURL = () => {
   };
 
   const executeShareURL = async (url: string) => {
+    if (
+      navigator.userAgent.toLowerCase().indexOf('kakaotalk') >= 0 &&
+      navigator.userAgent.toLowerCase().indexOf('android') >= 0
+    ) {
+      alert('카카오톡 인앱에서는 작동이 안될 수 있습니다.\r\n모바일 브라우저를 이용해주세요.');
+    }
+
     if (navigator.share) {
       await navigator.share({ title: 'CharmCharm', url });
       alert('복사되었습니다!');
@@ -16,13 +23,6 @@ const useShareURL = () => {
       await navigator.clipboard.writeText(url);
       alert('링크가 클립보드에 복사되었습니다!');
     } else {
-      if (
-        navigator.userAgent.toLowerCase().indexOf('kakaotalk') >= 0 ||
-        navigator.userAgent.toLowerCase().indexOf('android') >= 0
-      ) {
-        alert('카카오톡 인앱에서는 작동이 안될 수 있습니다.\r\n모바일 브라우저를 이용해주세요.');
-      }
-
       handleCopyClipBoard(url);
       alert('링크가 클립보드에 복사되었습니다!');
     }
