d3272885d66da814c072f3ee755fa0c458a05dc6
도비 <jm.dev.study@gmail.com>
2023-06-30 14:13:14 +0900

    Feat: 결과페이지에서 존재하지 않는 유저에 대한 에러 처리

--- src/pages/ResultPage/ErrorBoundary.tsx
+++ src/pages/ResultPage/ErrorBoundary.tsx
@@ -1,12 +1,25 @@
 import { AxiosError } from 'axios';
 import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-import ErrorNoResult from '@/components/ErrorNoResult';
+import ErrorNoResult from '@components/ErrorNoResult';
+import useSetRouter from '@hooks/useSetRouter';
 
 const ResultPageErrorFallback = ({ error }: { error: AxiosError<{ message: string }> }) => {
+  const { routerHelper } = useSetRouter();
+
   if (error.response?.data.message === '답변이 없습니다.') {
     return <ErrorNoResult />;
   }
+
+  useEffect(() => {
+    if (error.response?.data.message === '존재하지 않는 charmer') {
+      alert('존재하지 않는 유저입니다.');
+      return routerHelper.main();
+    }
+
+    alert(error.response?.data.message);
+  }, []);
 
   return <Navigate to="/" />;
 };
