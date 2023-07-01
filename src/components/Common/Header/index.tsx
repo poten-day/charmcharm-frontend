2e91e0ba467a69700d3dbc6235df2628411140af
도비 <jm.dev.study@gmail.com>
2023-07-02 02:58:41 +0900

    Style: Link 컴포넌트의 경로를 객체 형태가 아닌 일반 문자열로 변경

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="max-w-[480px] bg-primary500 p-[17px] text-white text-h3 font-BRBA_B">
-      <Link to={'/'} className="flex items-baseline justify-center gap-[10px]">
+      <Link to="/" className="flex items-baseline justify-center gap-[10px]">
         <Vector />
         나의 매력을 알려줘!
         <Vector />
--- src/router.tsx
+++ src/router.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/*',
-        element: <Navigate to={'/'} />,
+        element: <Navigate to="/" />,
       },
     ],
   },
