556b988326d6369136b4e60e19e713d28267da64
도비 <jm.dev.study@gmail.com>
2023-07-02 03:50:14 +0900

    Fix: 헤더의 Vector 이미지 너비 및 높이 지정

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -5,9 +5,9 @@ const Header = () => {
   return (
     <header className="max-w-[480px] bg-primary500 p-[17px] text-white text-h3 font-BRBA_B">
       <Link to="/" className="flex items-baseline justify-center gap-[10px]">
-        <Vector />
+        <Vector width={16} height={16} />
         나의 매력을 알려줘!
-        <Vector />
+        <Vector width={16} height={16} />
       </Link>
     </header>
   );
