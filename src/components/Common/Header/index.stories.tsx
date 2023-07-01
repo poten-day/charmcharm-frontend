8c4171f9fe4c36040f3f6b30d8377a882dffcd16
도비 <jm.dev.study@gmail.com>
2023-07-02 03:50:43 +0900

    Fix: Header 컴포넌트 스토리북 오류 수정

--- src/components/Common/Header/index.stories.tsx
+++ src/components/Common/Header/index.stories.tsx
@@ -1,9 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6';
 import Header from './index';
 
 const meta = {
   title: 'Common/Header',
   component: Header,
+  decorators: [withRouter],
+  parameters: {
+    reactRouter: {
+      routePath: '/',
+    },
+  },
 } satisfies Meta<typeof Header>;
 
 export default meta;
